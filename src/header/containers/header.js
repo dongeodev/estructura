468f4bd420009318f729e1dc15c932716655d5f1
DonGeo <geo.du07@gmail.com>
2020-11-28 14:06:47 -0500

    disable features

--- src/header/containers/header.js
+++ src/header/containers/header.js
@@ -1,37 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import logo from '../../assets/logo4.svg'
-import { Link } from 'react-router-dom'
+import logo from "../../assets/logo4.svg";
+import { Link } from "react-router-dom";
 
-class Header extends Component{
-  render(){
-    return(
+class Header extends Component {
+  render() {
+    return (
       <header>
-      <nav className= 'navbar navbar-expand-lg navbar-light bg-light shadow-sm' >
-        <Link className="navbar-brand col-1 col-lg-8" to='/'><img src={logo} className='logo'/></Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
+          <Link className="navbar-brand col-1 col-lg-8" to="/">
+            <img src={logo} className="logo" />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav"  >
-            <li className="nav-item mr-2">
-              <Link className="nav-link disabled" to='/blog' tabIndex="-1" aria-disabled="true" >Blog</Link>
-            </li>
-            <li className="nav-item mr-2" >
-              <Link className="nav-link" to='/services' >Servicios</Link>
-            </li>
-            <li className="nav-item ">
-              <Link className="nav-link" to='/portfolio'>Portafolio</Link>
-            </li>
-          </ul>
-        </div>
-       
-      </nav>
-    </header>
-
-    )
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav">
+              <li className="nav-item mr-2">
+                <Link
+                  className="nav-link disabled"
+                  to="/blog"
+                  tabIndex="-1"
+                  aria-disabled="true"
+                >
+                  Blog
+                </Link>
+              </li>
+              <li className="nav-item mr-2">
+                <Link className="nav-link disabled" to="/services">
+                  Servicios
+                </Link>
+              </li>
+              <li className="nav-item ">
+                <Link className="nav-link" to="/portfolio">
+                  Portafolio
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    );
   }
 }
 
-export default Header
+export default Header;
--- src/landing/components/footer.js
+++ src/landing/components/footer.js
@@ -1,48 +1,48 @@
-import React from 'react'
+import React from "react";
 
-import './footer.css'
-import logoBlanco from '../../assets/logow.svg'
-import linkedin from '../../assets/linkedin.svg'
-import Facebook from '../../assets/facebook-logo.svg'
-import Instagram from '../../assets/instagram.svg'
+import "./footer.css";
+import logoBlanco from "../../assets/logow.svg";
+import linkedin from "../../assets/linkedin.svg";
+import Facebook from "../../assets/facebook-logo.svg";
+import Instagram from "../../assets/instagram.svg";
 
-const Footer = (props)=>(
-  <div className='container-fluid bg-dark text-light'>
-    <div className='row Footer pt-4'>
-      <div className='col-4 col-md-4 text-center align-self-center'>
-        <img src={logoBlanco} alt='logo' className='logow'/>
-       
+const Footer = (props) => (
+  <div className="container-fluid bg-dark text-light">
+    <div className="row Footer pt-4">
+      <div className="col-4 col-md-4 text-center align-self-center">
+        <img src={logoBlanco} alt="logo" className="logow" />
       </div>
-      <div className='col-8 col-md-4 text-right'>
+      <div className="col-8 col-md-4 text-right">
         <h6>Quieres trabajar con nosotros?</h6>
         <p>Escribenos</p>
         <h6>Antioquia. Colombia</h6>
       </div>
-      <div className='col-12 col-md-4 mt-4 mt-md-0 text-center'>
+      <div className="col-12 col-md-4 mt-4 mt-md-0 text-center">
+        <div className="row text-center justify-content-center">
+          <div className="col-6 col-md-12">
+            <h5>Contacto</h5>
+            <p className="m-0">Tel. 300 000 00 00</p>
+          </div>
+          <h5 className="col-6 col-md-12 ">Nuestras Redes</h5>
 
-            
-        <div className='row text-center justify-content-center'>
-        <div className='col-6 col-md-12'>
-            <h5>Contacto</h5>  
-            <p className='m-0'>Tel. 3144877407</p>  
-        </div>
-            <h5 className='col-6 col-md-12 '>Nuestras Redes</h5>
-          
-          <div className='offset-6 offset-md-0 col-1 col-md-2 p-0  '>
-            <img src={linkedin} className='iconf'/>
+          <div className="offset-6 offset-md-0 col-1 col-md-2 p-0  ">
+            <img src={linkedin} className="iconf" />
           </div>
-          <div className='col-1 col-md-2 p-0'>
-          <img src={Facebook} className='iconf'/>
+          <div className="col-1 col-md-2 p-0">
+            <img src={Facebook} className="iconf" />
           </div>
-          <div className='col-1 col-md-2 p-0'>
-          <img src={Instagram} className='iconf'/>
+          <div className="col-1 col-md-2 p-0">
+            <img src={Instagram} className="iconf" />
           </div>
-
         </div>
       </div>
     </div>
-    <div className='col-12 text-center text-light pt-3 pb-3'><p className='font-weight-light m-0'>© 2019 Todos los derechos reservados. Desarrollado por <a>GeoDev</a></p></div>
+    <div className="col-12 text-center text-light pt-3 pb-3">
+      <p className="font-weight-light m-0">
+        © 2019 Todos los derechos reservados. Desarrollado por <a>DonGeoDev</a>
+      </p>
+    </div>
   </div>
-)
+);
 
-export default Footer
+export default Footer;
